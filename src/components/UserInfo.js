059c2a11ec03bb6b3b40eb897d78856597377e24
Maria Gorodilova <mashunja13@yandex.ru>
2023-05-27 01:18:56 +0500

    Пр9/2

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,23 +1,18 @@
 export default class UserInfo {
-  constructor({username, job, avatar}) {
-    this._username = document.querySelector(username);
-    this._job = document.querySelector(job);
-    this._avatar = document.querySelector(avatar);
+  constructor({usernameSelector, jobSelector}) {
+    this._userName = document.querySelector(usernameSelector);
+    this._userJob = document.querySelector(jobSelector);
   }
 
   getUserInfo(){
     return {
-      username: this._username.textContent,
-      job: this._job.textContent,
-      avatar: this._avatar.src
+      username: this._userName.textContent,
+      job: this._userJob.textContent
     }
-    
   }
   
-  setUserInfo(data) {
-    this._username.textContent = data.name;
-    this._job.textContent = data.about;
-    this._avatar.src = data.avatar;
+  setUserInfo(username, job) {
+    this._userName.textContent = username;
+    this._userJob.textContent = job;
   }
-  
 }
